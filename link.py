bdd1b2ff2ca3c70015c66a163e0979bc12e99ff0
SN0GWY <snowgy.green@gmail.com>
2023-08-16 00:34:11 -1200

    modify link

--- link.py
+++ link.py
@@ -1,14 +1,7 @@
-
-from asyncio.windows_events import NULL
-from base64 import standard_b64decode
-# import keyword
 import undetected_chromedriver as uc
-
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
-
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.action_chains import ActionChains
 import time
 
 
@@ -17,22 +10,18 @@ class link():
         self.mskill = mskill
         location = location
         skill = skill
-        date = int(date)
-        recent = 3600*24*date
-        # search_url = "https://www.monster.fr/emploi/recherche?q=python&where=paris&page=1&recency=today&so=m.h.s"
-        # recency=last+2+days
-        # recency=last+week
-        # last+2+weeks
-        # recency=last+month
-        # %2Creact
-        # https://fr.linkedin.com/jobs/search?keywords=php%2Creact&location=paris&geoId=&trk=public_jobs_jobs-search-bar_search-submit&position=1&pageNum=0
-        # https://fr.linkedin.com/jobs/search?keywords=Php&location=Paris%2C%20%C3%8Ele-de-France%2C%20France&locationId=&geoId=101240143&f_TPR=r86400&distance=25&position=1&pageNum=0
-        # https://fr.linkedin.com/jobs/search?keywords=Php&location=Paris%2C%20%C3%8Ele-de-France%2C%20France&locationId=&geoId=101240143&f_TPR=r604800&distance=25&position=1&pageNum=0
-        # https://fr.linkedin.com/jobs/search?keywords=php&location=paris&position=1&pageNum=0
-        # https://fr.linkedin.com/jobs/search?keywords=Php&location=Paris%2C%20%C3%8Ele-de-France%2C%20France&locationId=&geoId=101240143&f_TPR=r86400&distance=25&position=1&pageNum=0
-        # https://fr.linkedin.com/jobs/search?keywords=Php&location=Paris%2C%20%C3%8Ele-de-France%2C%20France&locationId=&geoId=101240143&f_TPR=r604800&distance=25&position=1&pageNum=0
-        # https://fr.linkedin.com/jobs/search?keywords=Php&location=Paris%2C%20%C3%8Ele-de-France%2C%20France&locationId=&geoId=101240143&f_TPR=r2592000&distance=25&position=1&pageNum=0
-
+        if date == '':
+            recent = ''
+        try:
+            date = int(date)
+            if date ==1:
+                recent =86400
+            if date > 1 and date <= 7:
+                recent = 604800
+            if date > 7 and date <= 30:
+                recent = 2592000
+        except: pass
+       
         search_url = f"https://fr.linkedin.com/jobs/search?keywords={self.mskill}&location={location}&position=1&f_TPR=r{recent}&pageNum=0"
         self.job= []
         op = uc.ChromeOptions()
@@ -42,7 +31,8 @@ class link():
         op.add_argument(f'--headless={True}')
 
         self.driver = uc.Chrome(options=op)
-        self.driver.set_window_size(600, 400)
+        self.driver.set_window_size(600, 900)
+        # self.driver.maximize_window()
         self.driver.execute_script(
                     "Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
         self.driver.get(search_url)
@@ -66,22 +56,36 @@ class link():
         divpanel = WebDriverWait(self.driver,10).until(EC.presence_of_element_located((By.XPATH, "//div[@class='base-serp-page__content']")))
         panel = WebDriverWait(self.driver,10).until(EC.presence_of_element_located((By.XPATH, "//ul[@class='jobs-search__results-list']")))
 
-        scroll_amount = 50  # Adjust this value as needed
-       
-        total_height = int(self.driver.execute_script("return document.body.scrollHeight"))
-        count = 0
         while True:
-            # Scroll down by scroll_amount pixels
-            self.driver.execute_script(f"window.scrollBy(0, {scroll_amount});")
-            count +=1
-            # Pause for a short duration to allow content to load
-            self.driver.implicitly_wait(10)
+            total_height = int(self.driver.execute_script("return document.body.scrollHeight"))
+            current_height = int(self.driver.execute_script("return window.scrollY;"))
+            temp1 = current_height
             
-            # Break the loop if you've reached the bottom of the content
-            if count >= total_height:
+            for i in range(0,total_height,10):
+                self.driver.execute_script(f"window.scrollBy(0, {i});")
+            time.sleep(1)
+            current_height = int(self.driver.execute_script("return window.scrollY;"))
+            temp2 = current_height
+            if temp2 == temp1:
                 break
+           
         self.driver.implicitly_wait(5)
-
+        try:
+            checknext=False
+            while not checknext:
+                print("continue")
+                WebDriverWait(self.driver,20).until(EC.presence_of_element_located((By.XPATH, "//*[@id='main-content']/section/button"))).click()
+                c_height = int(self.driver.execute_script("return window.scrollY;"))
+                t_height = int(self.driver.execute_script("return document.body.scrollHeight"))
+                tmep1 = c_height
+                step = int(tmep1/10)
+                time.sleep(1.2)
+                for j in range(0,t_height,step):
+                    self.driver.execute_script(f"window.scrollTo(0, {j});")
+        except:     
+            print("next")
+            checknext = True
+            pass
         joblist = panel.find_elements(By.TAG_NAME, "li")
         for i in joblist:
             monsterJob={}
