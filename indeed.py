6047ca95d72a83e4b0ec4ec0e00a6294f16fa843
SN0GWY <snowgy.green@gmail.com>
2023-08-16 00:33:59 -1200

    modify indeed

--- indeed.py
+++ indeed.py
@@ -1,19 +1,8 @@
-
-from asyncio.windows_events import NULL
-
-
 import undetected_chromedriver as uc
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
-
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.action_chains import ActionChains
 import time
-
-
-import pandas as pd
-
 
 class indeed(): 
     def init(self,location,skill,date,mskill):
@@ -21,8 +10,26 @@ class indeed():
         self.mskill = mskill
         location = location
         skill=skill
-        date = int(date)
-
+        m_date = ''
+        if date =='':
+            m_date = date
+        try:
+            date = int(date)
+            if date ==1:
+                m_date = 1
+            if date > 1 and date <= 3:
+                m_date = 3
+            if date > 3 and date <=7:
+                m_date = 7
+            if date >7 and date <=14:
+                m_date = 14
+            if date >14 and date <=30:
+                m_date = 'last'
+            if date > 30:
+                m_date = ''
+        except: pass
+        print(date)
+        
         self.job= []
         op = uc.ChromeOptions()
         custom_user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
@@ -36,8 +43,7 @@ class indeed():
                     "Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
 
 
-        # search_url = "https://fr.indeed.com/jobs?q=python%2Creact%2Cphp&l=Paris&fromage=1"
-        search_url = f"https://fr.indeed.com/jobs?q={self.mskill}&l={location}&fromage={date}"
+        search_url = f"https://fr.indeed.com/jobs?q={self.mskill}&l={location}&fromage={m_date}"
         self.driver.get(search_url)
         time.sleep(1)
         try:
@@ -47,6 +53,7 @@ class indeed():
             self.driver.switch_to.window(self.driver.window_handles[0])
         except: 
             pass
+        self.driver.get(search_url)
         self.active_flag = True
         self.driver.get(search_url)
         time.sleep(2)
@@ -62,7 +69,7 @@ class indeed():
                 except: pass
         except:
             pass
-            print("fetcherror")
+            print("")
             self.driver.quit()
         self.driver.quit()
         return self.job
@@ -74,14 +81,13 @@ class indeed():
             indeedjob = {}
             try:
                 adata = i.find_element(By.TAG_NAME, 'a')
+                # adata = i.find_element(By.TAG_NAME, 'a')
                 jobtitle = adata.get_attribute("id")
                 print(jobtitle)
                 if jobtitle == '':
                     try:
                         check = self.driver.find_element(By.XPATH, "//h3[@class='DesktopJobAlertPopup-heading']")
                         if check:
-                            print("ohmyugold")
-
                             WebDriverWait(self.driver,4).until(EC.presence_of_element_located((By.XPATH, "//button[@aria-label='fermer']"))).click()
                     except: pass
                 if jobtitle[:3] == "job" and jobtitle != '':
@@ -91,54 +97,27 @@ class indeed():
                     indeedjob["job_link"] = adata.get_attribute("href")
                     indeedjob["company"] = i.find_element(By.XPATH, ".//span[@class='companyName']").text
                     indeedjob["location"]= i.find_element(By.XPATH, ".//div[@class='companyLocation']").text
-                    # try:
-                    #     indeedjob["salary"] = i.find_element(By.XPATH, ".//div[@data-testid='attribute_snippet_testid']").text
-                    # except:
-                    #     indeedjob["salary"] = i.find_element(By.XPATH, ".//span[@class='estimated-salary']").text
-                    #     pass
                     tmp_date = i.find_element(By.XPATH, ".//div[@class='heading6 tapItem-gutter result-footer']/span[@class='date']").text
                     indeedjob["post_date"] = tmp_date[6:]
-                    # print("title------",indeedjob["title"])
-                    # print("job_link------",indeedjob["job_link"])
-                    # print("company------",indeedjob["company"])
-                    # print("location------",indeedjob["location"])
-                    # print("post_date------",indeedjob["post_date"])
-                    # print("salary------",indeedjob["salary"])
-                    # adata.click()
+                    print("title------",indeedjob["title"])
+                    print("job_link------",indeedjob["job_link"])
+                    print("company------",indeedjob["company"])
+                    print("location------",indeedjob["location"])
+                    print("post_date------",indeedjob["post_date"])
 
-                    # right = WebDriverWait(driver,10).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".jobsearch-RightPane")))
-                    # try:
-                    #     r_company_link=''
-                    #     r_company_link = WebDriverWait(right,6).until(EC.presence_of_element_located((By.XPATH, ".//div[@data-testid='inlineHeader-companyName']/span/a"))).get_attribute('href')
-                    #     # rdata = right.find_element(By.XPATH, "//div[@testid='inlineHeader-companyName']/span/a")
-                    #     print("company_link----",r_company_link)
-                    #     indeedjob["company_link"] = r_company_link
-                    # except:
-                    #     pass
-                    # try:
-                    #     tmp_jobdescription_p = WebDriverWait(right,30).until(EC.presence_of_element_located((By.XPATH, ".//div[@id='jobDescriptionText']")))
-                    #     print("ttttt",tmp_jobdescription_p)
-                    #     tmp_jobdescription_ptags = WebDriverWait(tmp_jobdescription_p, 30).until(EC.presence_of_all_elements_located((By.XPATH, ".//*")))
-                    #     tmp_des_text=''
-                    #     for j in tmp_jobdescription_ptags:
-                    #         if j.text:
-                    #             tmp_des_text += j.text
-                    #             print(j.text)
-                    #     indeedjob["job_description"] = tmp_des_text
-                    # except: pass
-                    
                     self.job.append(indeedjob)
             except:pass
         try: 
             self.count+=1
             print(self.count)
-            # WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.XPATH, f"//a[@aria-label='{self.count}']"))).click()
-            # print("next")
-            next = WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//a[@data-testid='pagination-page-next']")))
-            next.click()
+           
+            time.sleep(.6)
+            next = self.driver.find_element(By.XPATH, f"//a[@data-testid='pagination-page-{self.count}']")
+            nexturl = next.get_attribute('href')
+            self.driver.get(nexturl)
+            self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
         except: 
-            print("next error")
             self.active_flag = False
             pass
 
